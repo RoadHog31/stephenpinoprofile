0ba7f8379c4e81f897a6e9055c851700f61a3365
SPHyrule <stphnpino1@aol.com>
2021-05-12 12:58:27 +0100

    Add files via upload
    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# My Personal Website v1.0
+
+Project: Simple static website
+
+                HTML5, CSS3 & Vanilla JS                               
+                W3.CSS Framework
+                SoC
+
+Change Log Version 1.0
+
---
+++ js/badgesModal.js
@@ -0,0 +1,32 @@
+// Modal code for the Microsoft Badges.
+
+document.onload = function Main() {
+
+    openModal();
+    closeModal();
+    openModal2();
+    closeModal2();
+    
+  }
+
+  function openModal1(){
+
+    document.getElementById("idModal").style.display='block';
+
+  }
+
+  function closeModal1(){
+
+    document.getElementById("idModal").style.display="none";
+  }
+
+  function openModal2(){
+
+    document.getElementById("idModal2").style.display='block';
+
+  }
+
+  function closeModal2(){
+
+    document.getElementById("idModal2").style.display="none";
+  }
